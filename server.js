db026d252d91c6794cea31fc8f2eec2c16cce742
Brandon <bmw8719@gmail.com>
2019-03-21 14:56:08 -0500

    Restart dev server on change

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
+
+app.get('/api/ping', (req, res) => {
+	res.send('hello world!')
+})
+
+// Serve Vue Files
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 8080;
 app.listen(port);
